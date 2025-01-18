da4dbb91f1cbc4bb106f09aaa27ff60fcea7976d
Jaheim Campbell <jaheimkcampbell@gmail.com>
2025-01-18 18:05:50 -0500

    Initial upload of website files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Prices for the shirts
+const price1 = 4000;
+const price2 = 4500;
+
+// Set prices dynamically
+document.getElementById('price1').textContent = price1;
+document.getElementById('price2').textContent = price2;
